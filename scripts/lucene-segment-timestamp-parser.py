8234149fb41dbddbc5d6ac517629b7428220c55c
Ian Hoang <hoangia@amazon.com>
2024-02-14 14:54:21 -0600

    Refactor lucene_segment_timestamp_parser.py and rename it to lucene-segment-timestamp-parser.py
    Signed-off-by: Ian Hoang <hoangia@amazon.com>

--- scripts/lucene-segment-timestamp-parser.py
+++ scripts/lucene-segment-timestamp-parser.py
@@ -1,31 +1,39 @@
 import re
 import datetime
+import argparse
 
 MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 text = "0 [897249601000,897762353000] - 25846743"
 
 
-def main():
-    file_lines = read_file("/Users/hoangia/Desktop/Development/segments-experiment-3.txt")
+def main(filename):
+    file_lines = read_file(filename)
+    converted_lines = parse_and_convert_lucene_segment_timestamps(file_lines)
+    filename = filename.split(".")[0]
+    with open(f"converted-{filename}.txt", "w") as file:
+        for line in converted_lines:
+            file.write(line)
+
+    print(f"Finished converting lucene segment timestamps into human-readable formatted timestamps for {filename}")
+
+def parse_and_convert_lucene_segment_timestamps(file_lines):
     converted_lines = []
 
-    print(file_lines)
     for line in file_lines:
         if "[" in line:
             # Find iterations
             search_results = re.finditer(r'\[(.*?)\]', line)
-
             raw_results = (search_results.__next__()).group(0)
-            # Convert to list
             results = raw_results.lstrip("[]").rstrip("]").split(",")
 
-            # Convert times to UTC
+            # Each segment has a start time and end time. Convert those times to UTC and store as a list, separating date and time
             start_date = (datetime.datetime.utcfromtimestamp(int(results[0][0:-3])).strftime('%m-%d-%Y %H:%M:%S')).split(" ")
             end_date = (datetime.datetime.utcfromtimestamp(int(results[1][0:-3])).strftime('%m-%d-%Y %H:%M:%S')).split(" ")
 
-            start_date = f"{convert_date_to_human_readable_format(start_date[0])} {start_date[1]}"
-            end_date = f"{convert_date_to_human_readable_format(end_date[0])} {end_date[1]}"
-            converted_dates = f"[{start_date}, {end_date}]"
+            reformatted_start_date = f"{convert_date_to_human_readable_format(start_date[0])} {start_date[1]}"
+            reformatted_end_date = f"{convert_date_to_human_readable_format(end_date[0])} {end_date[1]}"
+
+            converted_dates = f"[{reformatted_start_date}, {reformatted_end_date}]"
 
             # Substitute times
             converted_line = re.sub(r'\[(.*?)\]', converted_dates, line)
@@ -33,9 +41,8 @@ def main():
         else:
             converted_lines.append(line)
 
-    with open("converted-segments-experiment-3.txt", "w") as file:
-        for line in converted_lines:
-            file.write(line)
+    return converted_lines
+
 
 def convert_date_to_human_readable_format(input_date):
     date_components = input_date.split("-")
@@ -52,4 +59,8 @@ def read_file(file):
     return lines
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(prog="Lucene Segment Timestamp Parser", description="A tool to convert lucene segment epoch timestamps to human-readable formatted timestamps")
+    parser.add_argument("--file", "-f", required=True, help="File containing lucene segments timestamps. Usually outputted by executing VisualizePointTree.java from Lucene-University.")
+    args = parser.parse_args()
+    file = args.file
+    main(file)
