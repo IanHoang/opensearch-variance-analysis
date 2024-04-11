7639cf11d1b867edb9f770c0ea7dfa5720b9e14e
Ian Hoang <hoangia@amazon.com>
2024-04-11 11:16:12 -0500

    Add working scripts for segment tuning analysis from scenario 4. Runs create index, updates index settings, then runs test all in 3 iterations
    Signed-off-by: Ian Hoang <hoangia@amazon.com>

---
+++ scripts/segment-tuning-scenario-v2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+host=""
+workload="http_logs"
+# No force merge
+id="segment-tuning-scenario-4"
+tag="test-type:segment-tuning-scenario-4"
+workload_params="target_throughput:none,bulk_indexing_clients:1"
+
+for i in {1..3}
+do
+    echo "Creating Index"
+    run_id="${id}-create-index-only-${i}"
+    opensearch-benchmark execute-test --workload=$workload --pipeline=benchmark-only --target-hosts=$host --telemetry=node-stats --test-execution-id=$run_id --workload-params=$workload_params --include-tasks="delete-index,create-index,check-cluster-health" --test-mode
+
+    echo "Sleeping for 30 seconds"
+    sleep 30
+
+    echo "Updating index settings"
+    ./update-index-settings.sh $host $run_id
+
+    echo "Sleeping for 30 seconds"
+    sleep 30
+
+    echo "Running Test"
+    run_id="${id}-run-${i}"
+    opensearch-benchmark execute-test --workload=$workload --pipeline=benchmark-only --target-hosts=$host --telemetry=node-stats --test-execution-id=$run_id --workload-params=$workload_params --include-tasks="check-cluster-health,index-append,desc_sort_size,asc_sort_size,desc_sort_timestamp,asc_sort_timestamp,desc_sort_with_after_timestamp,asc_sort_with_after_timestamp" --test-mode
+
+done
+
---
+++ scripts/update-index-settings.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+host=$1
+id=$2
+
+echo "Running update on $host for $id"
+
+echo "Updating max merged segment"
+
+response=$(curl --write-out '%{http_code}' --silent --output /dev/null \
+  -XPUT "${host}/logs-241998/_settings" \
+  -H 'Content-Type: application/json' \
+  -d '{
+    "index" : {
+      "merge.policy.max_merged_segment": "2gb"
+    }
+  }')
+
+if [ "$response" -eq 200 ]; then
+    echo "Success! The response code was 200 (OK)."
+else
+    echo "Error: The response code was $response."
+    exit 1
+fi
+
+echo "Updating floor segment"
+response_2=$(curl --write-out '%{http_code}' --silent --output /dev/null \
+  -XPUT "${host}/logs-241998/_settings" \
+  -H 'Content-Type: application/json' \
+  -d '{
+    "index" : {
+      "merge.policy.floor_segment": "500mb"
+    }
+  }')
+
+  if [ "$response_2" -eq 200 ]; then
+    echo "Success! The response code was 200 (OK)."
+else
+    echo "Error: The response code was $response_2."
+    exit 1
+fi
+
+echo "Checking Settings"
+check_settings=$(curl -XGET "${host}/logs-241998/_settings?pretty")
+echo $check_settings > $id-index-settings.json
+
+echo "Finished checking settings"
