768ca01f5f66324eba3beef1e034693f48539694
Ian Hoang <hoangia@amazon.com>
2024-04-11 17:15:28 -0500

    Improved scripts for scenario 5 and update-index-settings
    Signed-off-by: Ian Hoang <hoangia@amazon.com>

--- scripts/segment-tuning-scenario-v2.sh
+++ scripts/segment-tuning-scenario-v2.sh
@@ -2,9 +2,16 @@
 host=""
 workload="http_logs"
 # No force merge
-id="segment-tuning-scenario-4"
-tag="test-type:segment-tuning-scenario-4"
+id="segment-tuning-scenario-5"
+tag="test-type:segment-tuning-scenario-5"
 workload_params="target_throughput:none,bulk_indexing_clients:1"
+
+err_exit() {
+    echo "${0##*/}: $1" >&2
+    exit 1
+}
+
+mkdir ~/$id
 
 for i in {1..3}
 do
@@ -16,7 +23,7 @@ do
     sleep 10
 
     echo "Updating index settings"
-    ./update-index-settings.sh $host $run_id
+    ./update-index-settings-v2.sh -h $host -i $run_id -fst
 
     echo "Sleeping for 10 seconds"
     sleep 10
@@ -26,7 +33,8 @@ do
     opensearch-benchmark execute-test --workload=$workload --pipeline=benchmark-only --target-hosts=$host --telemetry=node-stats --test-execution-id=$run_id --workload-params=$workload_params --include-tasks="check-cluster-health,index-append,desc_sort_size,asc_sort_size,desc_sort_timestamp,asc_sort_timestamp,desc_sort_with_after_timestamp,asc_sort_with_after_timestamp"
 
     echo "Finished running iteration. Moving test execution file to home directory"
-    cp ~/.benchmark/benchmarks/test_executions/$run_id/test_execution.json ~/$run_id.json
+    cp ~/.benchmark/benchmarks/test_executions/$run_id/test_execution.json ~/$run_id.json || err_exit "Cannot cp file $run_id/test_execution.json over to home directory"
+    mv ~/$run_id.json ~/$id || err_exit "Cannot move $run_id.json"
 
     echo "Sleeping for 10 seconds"
     sleep 10
---
+++ scripts/update-index-settings-v2.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+
+usage() {
+    echo "Usage: $0 -h HOST -i ID [-f flag for updating floor segment size] [-s flag for updating segment size] [-m flag for merge policy to log_byte_size] [-t flag for setting segments per tier to 5]"
+}
+
+err_exit() {
+    echo "${0##*/}: $1" >&2
+    exit 1
+}
+
+update_max_merged_segment() {
+  echo "Updating max merged segment to 2gb"
+
+  response=$(curl --write-out '%{http_code}' --silent --output /dev/null \
+    -XPUT "${host}/logs-241998/_settings" \
+    -H 'Content-Type: application/json' \
+    -d '{
+      "index" : {
+        "merge.policy.max_merged_segment": "2gb"
+      }
+    }')
+
+  if [ "$response" -eq 200 ]; then
+      echo "Success! The response code was 200 (OK)."
+  else
+      echo "Error: The response code was $response."
+      exit 1
+  fi
+}
+
+update_floor_segment_size() {
+  echo "Updating floor segment size to 500mb"
+
+  response=$(curl --write-out '%{http_code}' --silent --output /dev/null \
+    -XPUT "${host}/logs-241998/_settings" \
+    -H 'Content-Type: application/json' \
+    -d '{
+      "index" : {
+        "merge.policy.floor_segment": "500mb"
+      }
+    }')
+
+  if [ "$response" -eq 200 ]; then
+      echo "Success! The response code was 200 (OK)."
+  else
+      echo "Error: The response code was $response."
+      exit 1
+  fi
+}
+
+update_merge_policy_to_log_byte_size() {
+  echo "Updating merge policy to log_byte_size"
+
+  response=$(curl --write-out '%{http_code}' --silent --output /dev/null \
+    -XPUT "${host}/logs-241998/_settings" \
+    -H 'Content-Type: application/json' \
+    -d '{
+      "indices" : {
+          "time_series_index.default_index_merge_policy": "log_byte_size"
+      }
+    }')
+
+  if [ "$response" -eq 200 ]; then
+      echo "Success! The response code was 200 (OK)."
+  else
+      echo "Error: The response code was $response."
+      exit 1
+  fi
+}
+
+update_segments_per_tier() {
+  echo "Update segments per tier to 5"
+
+  response=$(curl --write-out '%{http_code}' --silent --output /dev/null \
+    -XPUT "${host}/logs-241998/_settings" \
+    -H 'Content-Type: application/json' \
+    -d '{
+      "index" : {
+        "merge.policy.segments_per_tier": "5"
+      }
+    }')
+
+  if [ "$response" -eq 200 ]; then
+      echo "Success! The response code was 200 (OK)."
+  else
+      echo "Error: The response code was $response."
+      exit 1
+  fi
+}
+
+# Initialize variables
+host=""
+id=""
+floor_segment_size=false
+segment_size=false
+merge_policy=false
+segments_per_tier=false
+
+# Parse command-line options
+while getopts ":h:i:fsmt" opt; do
+    case $opt in
+        h)
+            host=$OPTARG
+            ;;
+        i)
+            id=$OPTARG
+            ;;
+        f)
+            floor_segment_size=true
+            ;;
+        s)
+            segment_size=true
+            ;;
+        m)
+            merge_policy=true
+            ;;
+        t)
+            segments_per_tier=true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+# Shift the argument pointer to skip the processed options
+shift $((OPTIND - 1))
+
+# Check if required options are provided
+if [ -z "$host" ] || [ -z "$id" ]; then
+    echo "The -h and -i options are required." >&2
+    usage
+    exit 1
+fi
+
+# Check if at least one of -f, -s, or -m is provided
+if [ "$floor_segment_size" = false ] && [ "$segment_size" = false ] && [ "$merge_policy" = false ] && [ "$segments_per_tier" = false ]; then
+    echo "At least one of -f, -s, -m, or -t must be provided." >&2
+    usage
+    exit 1
+fi
+
+echo "Running update on $host for $id."
+
+# Check which flags were provided
+if $floor_segment_size; then
+    echo "Flag -f was provided."
+    update_floor_segment_size
+fi
+
+if $segment_size; then
+    echo "Flag -s was provided."
+    update_max_merged_segment
+fi
+
+if $merge_policy; then
+    echo "Flag -m was provided."
+    update_merge_policy_to_log_byte_size
+fi
+
+if $segments_per_tier; then
+    echo "Flag -t was provided."
+    update_segments_per_tier
+fi
+
+echo "Checking Settings"
+check_settings=$(curl -XGET "${host}/logs-241998/_settings?pretty")
+echo $check_settings > $id-index-settings.json
+
+echo "Finished checking settings"
