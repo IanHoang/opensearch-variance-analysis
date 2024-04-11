cbc3d3ff35fbf3753a740b43538c9a35beddf2cd
Ian Hoang <hoangia@amazon.com>
2024-04-11 11:55:53 -0500

    Remove test mode
    Signed-off-by: Ian Hoang <hoangia@amazon.com>

--- scripts/segment-tuning-scenario-v2.sh
+++ scripts/segment-tuning-scenario-v2.sh
@@ -10,7 +10,7 @@ for i in {1..3}
 do
     echo "Creating Index"
     run_id="${id}-create-index-only-${i}"
-    opensearch-benchmark execute-test --workload=$workload --pipeline=benchmark-only --target-hosts=$host --telemetry=node-stats --test-execution-id=$run_id --workload-params=$workload_params --include-tasks="delete-index,create-index,check-cluster-health" --test-mode
+    opensearch-benchmark execute-test --workload=$workload --pipeline=benchmark-only --target-hosts=$host --telemetry=node-stats --test-execution-id=$run_id --workload-params=$workload_params --include-tasks="delete-index,create-index,check-cluster-health"
 
     echo "Sleeping for 10 seconds"
     sleep 10
@@ -23,7 +23,7 @@ do
 
     echo "Running Test"
     run_id="${id}-run-${i}"
-    opensearch-benchmark execute-test --workload=$workload --pipeline=benchmark-only --target-hosts=$host --telemetry=node-stats --test-execution-id=$run_id --workload-params=$workload_params --include-tasks="check-cluster-health,index-append,desc_sort_size,asc_sort_size,desc_sort_timestamp,asc_sort_timestamp,desc_sort_with_after_timestamp,asc_sort_with_after_timestamp" --test-mode
+    opensearch-benchmark execute-test --workload=$workload --pipeline=benchmark-only --target-hosts=$host --telemetry=node-stats --test-execution-id=$run_id --workload-params=$workload_params --include-tasks="check-cluster-health,index-append,desc_sort_size,asc_sort_size,desc_sort_timestamp,asc_sort_timestamp,desc_sort_with_after_timestamp,asc_sort_with_after_timestamp"
 
     echo "Finished running iteration. Moving test execution file to home directory"
     cp ~/.benchmark/benchmarks/test_executions/$run_id/test_execution.json ~/$run_id.json
